3df71c52a4ecd4d3d1953c27c9ebeb590e282cc7
Dan Moloney <daniel.moloney@sysdig.com>
2022-06-02 12:48:03 +1000

    fix

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 # Create compute resources
 module "compute" {
-  source              = "./modules/compute"
-  vsphere_user        = var.vsphere_user
-  vsphere_password    = var.vsphere_password
-  vsphere_server      = var.vsphere_server
-  vm_name_prefix      = var.vm_name_prefix
+  source               = "./modules/compute"
+  vsphere_user         = var.vsphere_user
+  vsphere_password     = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  vm_name_prefix       = var.vm_name_prefix
+  vm_worker_count      = var.vm_worker_count
 }
