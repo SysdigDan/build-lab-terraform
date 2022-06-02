5eef13d57de4ded6ffef35a026941fb50d209fdf
Dan Moloney <daniel.moloney@sysdig.com>
2022-06-02 12:20:01 +1000

    update compute module

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -7,55 +7,46 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "dc" {
+data "vsphere_datacenter" "datacenter" {
   name = "LAB"
 }
 
 data "vsphere_datastore" "datastore" {
   name          = "LAB-VM-01"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
     name          = "RESOURCES"
-    datacenter_id = "${data.vsphere_datacenter.dc.id}"
+    datacenter_id = data.vsphere_datacenter.datacenter.id
 }
-
-# data "vsphere_resource_pool" "pool" {
-#   name          = "RESOURCES"
-#   datacenter_id = "${data.vsphere_datacenter.dc.id}"
-# }
 
 data "vsphere_network" "network" {
   name          = "lab_management_6"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = "ubuntu-20.04.2"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
   folder           = "Terraform"
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  firmware         = "${data.vsphere_virtual_machine.template.firmware}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus = 2
   memory   = 4096
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
-
-    network_interface {
-        network_id   = "${data.vsphere_network.network.id}"
-        adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
-    }
-
-    disk {
-        label            = "disk0"
-        size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-        eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-        thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
-    }
-    scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  disk {
+      label            = "disk0"
+      size             = data.vsphere_virtual_machine.template.disks.0.size
+      thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+  network_interface {
+      network_id   = data.vsphere_network.network.id
+      adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
 }
