96d8254d31f54e6fa6deacda8f908664f7ed310c
Dan Moloney <daniel.moloney@sysdig.com>
2022-06-02 12:31:46 +1000

    testing

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,13 @@
+variable "name_prefix" {
+  default = "srvlb"
+}
+variable "offset" {
+  default = 1
+}
+variable "start_ipv4_address" {
+  default = 100
+}
+
 provider "vsphere" {
   user           = var.vsphere_user
   password       = var.vsphere_password
@@ -13,40 +23,68 @@ data "vsphere_datacenter" "datacenter" {
 
 data "vsphere_datastore" "datastore" {
   name          = "LAB-VM-01"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
 }
 
 data "vsphere_compute_cluster" "cluster" {
-    name          = "RESOURCES"
-    datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = "RESOURCES"
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = format("%s%s", data.vsphere_compute_cluster.cluster.name, "/Resources")
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
 }
 
 data "vsphere_network" "network" {
   name          = "lab_management_6"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = "ubuntu-20.04.2"
-  datacenter_id = data.vsphere_datacenter.datacenter.id
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
+  count            = 1
+  name             = "${var.name_prefix}${format("%02d", count.index + 1 + var.offset)}"
   folder           = "Terraform"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  firmware         = "${data.vsphere_virtual_machine.template.firmware}"
+
   num_cpus = 2
   memory   = 4096
-  guest_id = data.vsphere_virtual_machine.template.guest_id
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
-  # disk {
-  #     label            = "disk0"
-  #     size             = data.vsphere_virtual_machine.template.disks.0.size
-  #     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
-  # }
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
   network_interface {
-      network_id   = data.vsphere_network.network.id
-      adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    customize {
+      linux_options {
+        host_name = "${var.name_prefix}${format("%02d", count.index + 1 + var.offset)}"
+        domain    = "prod.com"
+      }
+	  network_interface {
+        ipv4_address = "${format("10.0.0.%d", (count.index + 1 + var.offset + var.start_ipv4_address))}"
+        ipv4_netmask = "24"
+      }
+
+      ipv4_gateway = "10.0.0.1"
+	    dns_suffix_list = ["prod.com"]
+      dns_server_list = ["10.0.0.2"]
+    }
   }
 }
