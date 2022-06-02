f1c08185aa913d3bd2ffb6e1a260a91427262e53
Dan Moloney <daniel.moloney@sysdig.com>
2022-06-02 12:37:28 +1000

    fix vm_name_prefix

--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ module "compute" {
   vsphere_user        = var.vsphere_user
   vsphere_password    = var.vsphere_password
   vsphere_server      = var.vsphere_server
+  vm_name_prefix      = var.vm_name_prefix
 }
